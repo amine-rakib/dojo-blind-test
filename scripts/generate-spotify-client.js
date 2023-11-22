5c01e30d3e09b83d252be674e4e02dc9b322fffb
thomas.eudes <teudes@ginkgobioworks.com>
2023-11-22 09:46:53 +0100

    Create script

---
+++ scripts/generate-spotify-client.js
@@ -0,0 +1,8 @@
+import openapi from "../openapi.json" assert { type: 'json' };; 
+
+console.log("\nLaunched generate-spotify-client script");
+console.log('Generating Spotify client from OpenApi spec file...\n')
+
+
+// TO DO: write script
+// console.log(openapi)
