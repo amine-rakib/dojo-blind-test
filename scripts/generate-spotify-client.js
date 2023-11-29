cbe793f30903e3f51aabaf3369a59f353e162916
thomas.eudes <teudes@ginkgobioworks.com>
2023-11-29 10:00:54 +0100

    add initial script

--- scripts/generate-spotify-client.js
+++ scripts/generate-spotify-client.js
@@ -1,8 +1,34 @@
-import openapi from "../openapi.json" assert { type: 'json' };; 
+import { mkdir, writeFile } from "fs/promises";
 
-console.log("\nLaunched generate-spotify-client script");
-console.log('Generating Spotify client from OpenApi spec file...\n')
+import openapi from "../openapi.json" assert { type: 'json' };
 
+const targetDirectory = "src/lib/spotify/model";
 
-// TO DO: write script
-// console.log(openapi)
+const generateSpotifyClient = async () => {
+  console.log("\nLaunched generate-spotify-client script");
+  console.log('Generating Spotify client from OpenApi spec file...\n')
+  await mkdir(targetDirectory, { recursive: true }); // Generate target directory
+
+  const schemas = openapi.components.schemas;
+  const typesToGenerate = Object.keys(schemas);
+
+  for (const typeName of typesToGenerate) {
+    const typeSchema = schemas[typeName];
+    generateType(typeName, typeSchema);
+  }
+}
+
+const generateType = (typeName, typeSchema) => {  
+  console.log(`Generating type ${typeName}...`);
+
+  const generatedType = getGeneratedType(typeName, typeSchema);
+
+  writeFile(`${targetDirectory}/${typeName}.ts`, generatedType);
+}
+
+const getGeneratedType = (type, schema) => {
+  // TO DO: Generate typescript code from schema
+  return "";
+}
+
+generateSpotifyClient();
