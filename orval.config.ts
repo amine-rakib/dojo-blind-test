52228739cf3b8288c97eea49e5231ec23612f074
thomas.eudes <teudes@ginkgobioworks.com>
2024-01-08 11:27:10 +0100

    add baseUrl to orval config

--- orval.config.ts
+++ orval.config.ts
@@ -2,10 +2,11 @@ module.exports = {
     spotify: {
       output: {
         mode: 'tags-split',
-        target: 'src/lib/spotify/api/spotify.ts',
+        target: 'src/lib/spotify/api',
         schemas: 'src/lib/spotify/model',
         client: 'swr',
-        mock: true,
+        mock: false,
+        baseUrl: 'https://api.spotify.com/v1',
       },
       input: {
         target: './openapi.json',
